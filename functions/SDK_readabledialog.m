2a62f9f501335e506f261b77b5fe7574ca2f5b5c
Stefan Marien <stefan.marien@medtronic.com>
2016-02-08 11:21:37 +0100

    fixed: SDK_readabledialog.m: reference to readable flag now has correct location

--- functions/SDK_readabledialog.m
+++ functions/SDK_readabledialog.m
@@ -2,13 +2,13 @@ function [ output ] = SDK_readabledialog( S )
 %SDK_READABLE Summary of this function goes here
 %   Detailed explanation goes here
 
-if S.readable
+if S.developerFlags.readable
     switch questdlg(sprintf('This modification might make your session incompatible with SureTune.\nDo you wish to continue?'),'SureTune warning','Yes','Yes to all','No','No') 
         case 'Yes'
             output = 1;
             S.addtolog('The following action might make this session incompatible with Suretune:')
         case 'Yes to all'
-            S.readable = 0;
+            S.developerFlags.readable = 0;
             output = 1;
             S.addtolog('The following action might make this session incompatible with Suretune:')
         case 'No'
