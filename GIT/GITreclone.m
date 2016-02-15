d39ce160de01ae2f96696aa387f47073b351c5d1
Jonas <jonas.roothans@medtronic.com>
2016-02-15 12:06:51 +0100

    Added GIT commands to rebase from matlab

---
+++ GIT/GITreclone.m
@@ -0,0 +1,14 @@
+button = questdlg(sprintf('You are about to reclone the lastest version of the SDK.\n Do you wish to keep your local changes? \n(If you press no, all local changes are deleted) '),'Do you want to proceed?','Cancel') ;
+    switch button
+        case 'Yes'
+            !git stash
+            !git pull
+            !git stash pop
+        case 'No'
+            !git reset --hard
+            !git pull
+        case 'Cancel'
+            error('Script aborted - User pressed cancel.')
+        case ''
+           error('Script aborted - User pressed cancel.')
+    end
