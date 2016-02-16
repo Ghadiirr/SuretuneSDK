781c7d218ce1e83a711b8fc0e55fb7775b373060
Jonas <jonas.roothans@medtronic.com>
2016-02-16 16:52:54 +0100

    Fixed loadSDK: the GIT-check was performed in the current directory, instead of the SDK dir.

--- loadSDK.m
+++ loadSDK.m
@@ -5,10 +5,11 @@ addpath(genpath(installationdir));
 clc
 fprintf('SDK is ready to use!\nTo get started run helpSDK.\n\n~ Kind regards, Jonas \n\t\tjonas.roothans@medtronic.com\n\n(This is still work in progress, so please contact me if you have any difficulties)\n')
 
-
+thisdir = pwd;
+cd (installationdir)
 [~,message] = system('git fetch --verbose');
-if ~strfind(message,'[up to date]')
+if isempty(strfind(message,'[up to date]'))
     GITreclone
 end
-    
+    cd(thisdir)
 
